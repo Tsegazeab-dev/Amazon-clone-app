250c8b38443f49ede1d87588c7df14faf092bb03
Tsegazeab <tsegazeabwuta@gmail.com>
2024-02-28 10:34:00 -0800

    update base url to deploy the frontend apart from the backend

--- Amazon-clone/src/utils/axios.js
+++ Amazon-clone/src/utils/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://amazon-backend-s6nl.onrender.com",
   // http://127.0.0.1:5000
-  // https://amazon-backend-s6nl.onrender.com (render backend)
+  // https://amazon-backend-s6nl.onrender.com (backend url deployed on render)
   // baseURL: "http://127.0.0.1:5001/clone-97f4e/us-central1/api" (firebase backend)
 });
